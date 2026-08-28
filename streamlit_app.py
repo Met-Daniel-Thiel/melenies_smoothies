96677f9dad2f8b3398ec413a84a4b32c1fbd9402
Daniel Thiel <daniel.thiel@metoffice.gov.uk>
2026-08-28 19:47:56 +0100

    lesson update

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,6 +39,9 @@ if ingreditents_list:
         smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
+        my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+                values ('""" + ingredients_string + """','"""+name_on_order+ """')"""
+
         time_to_insert = st.button('Submit Order')
 
         if time_to_insert:
