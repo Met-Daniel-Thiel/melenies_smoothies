db7e5e636c4604516976899c20fb795fab52c1bc
Daniel Thiel <daniel.thiel@metoffice.gov.uk>
2026-08-28 19:43:40 +0100

    lesson update

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,5 +36,5 @@ if ingreditents_list:
         st.write('The search value for ', fruit_chosen, ' is: ', search_on, '.')
 
         st.subheader(fruit_chosen + ' Nutritional Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
