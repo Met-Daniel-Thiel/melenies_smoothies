c0d0e872ca597dc948d96a1091e5675418f98cba
Daniel Thiel <daniel.thiel@metoffice.gov.uk>
2026-08-28 18:36:42 +0100

    title correction

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
-st.title(f"My Parents New Healthy Diner")
+st.title(f":cup_with_straw: Customise Your Smoothie :cup_with_straw: {st.__version__}")
 st.write("Choose the fruits you want in your custom Smoothie!")
 
 name_on_order = st.text_input('Name on Smoothie:')
