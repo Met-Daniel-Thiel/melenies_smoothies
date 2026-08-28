74f9f93d75dce9780a6c385c6771073fc7aeff8d
Daniel Thiel <daniel.thiel@metoffice.gov.uk>
2026-08-28 19:10:21 +0100

    lesson update

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,6 +46,6 @@ if ingreditents_list:
     ingredients_string = ''
     for fruit_chosen in ingreditents_list:
         ingredients_string += fruit_chosen + ' '
-        st.subheader(fruit chosen: + ' Nutritional Information')
+        st.subheader(fruit_chosen: + ' Nutritional Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
